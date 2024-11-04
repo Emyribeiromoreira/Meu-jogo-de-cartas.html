0e78c88e3829daa99cff26f3d76ee04dc635984b
Emyribeiromoreira <132688563+Emyribeiromoreira@users.noreply.github.com>
2024-11-04 08:42:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criarCartao(
+    'Educação fisica',
+    'O volei serve para que?',
+    'Melhorar a cognicao porque necessita ter a percepcao rapida da jornada'
+)
+
+criarCartao(
+    'Cinderela',
+    'a cor do vestido de cinderela?',
+    'Branco'
+)
+
+criarCartao(
+    'Matemática',
+    'Quanto é 13*379',
+    '4.927'
+)
